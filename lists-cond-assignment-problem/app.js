567385b9016fd887e31ab923b4abc4556ae67ccc
unknown <bryanchua0724@gmail.com>
2021-10-30 21:13:34 +0800

    Lesson 3 - Rendering Content Conditionally

--- lists-cond-assignment-problem/app.js
+++ lists-cond-assignment-problem/app.js
@@ -1,7 +1,15 @@
 const app = Vue.createApp({
     data() {
         return {
+            EnteredTaskValue: '',
             Tasks: []
         };
     },
+    methods: {
+        AddTasks() {
+            this.Tasks.push(this.EnteredTaskValue);
+        }
+    }
 });
+
+app.mount('#assignment');

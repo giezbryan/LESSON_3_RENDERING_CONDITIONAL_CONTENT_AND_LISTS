822088efe9b6d90460ae262e124697bd03f8ba93
unknown <bryanchua0724@gmail.com>
2021-10-30 22:17:27 +0800

    Lesson 3 - Hiding and Revealing the List

--- lists-cond-assignment-problem/app.js
+++ lists-cond-assignment-problem/app.js
@@ -2,12 +2,23 @@ const app = Vue.createApp({
     data() {
         return {
             EnteredTaskValue: '',
-            Tasks: []
+            Tasks: [],
+            Button: true,
+            HideButton: 'Hide'
         };
     },
     methods: {
         AddTasks() {
             this.Tasks.push(this.EnteredTaskValue);
+        },
+        HideAndReveal() {
+            this.Button = !this.Button;
+            if (this.Button === true) {
+                this.HideButton = 'Hide';
+                this.Button = 'Tago';
+            } else {
+                this.HideButton = 'Show List'
+            }
         }
     }
 });
